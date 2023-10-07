e0d650c696474039679f1602b832bbf795c3fe7b
yatinhudda <yatinchaudhary72@gmail.com>
2023-10-07 12:14:52 +0530

    Get methods of any class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +10,15 @@ public class Main {
         Field[] f = Game.class.getDeclaredFields();
         for(Field field: f){
             System.out.println(field.getClass().getName());
+        }
+
+        Object o = new Game(2, "Car race", 1);
+        System.out.println(o.getClass().getName());
+
+        Class<?> c = o.getClass();
+        Method[] methods = c.getDeclaredMethods();
+        for(Method method : methods){
+            System.out.println(method.getName());
         }
     }
 }
