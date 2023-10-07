ecbeee9c737a41b94e8cb8b669df08aefc0e1f8d
yatinhudda <yatinchaudhary72@gmail.com>
2023-10-07 11:36:30 +0530

    Some meta class intro

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -1,4 +1,37 @@
 package org.example;
 
 public class Game {
+
+    private int gameId;
+    private String gameName;
+    private int version;
+
+    public Game(int gameId, String gameName, int version){
+        this.gameId = gameId;
+        this.gameName = gameName;
+        this.version = version;
+    }
+    public void setGameId(int gameId){
+        this.gameId = gameId;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public int getGameId() {
+        return gameId;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public int getVersion() {
+        return version;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.lang.reflect.Field;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Game mario = new Game(1, "Mario", 1);
+        mario.getClass();
+        Field[] f = Game.class.getDeclaredFields();
+        for(Field field: f){
+            System.out.println(field.getClass().getName());
+        }
     }
 }
